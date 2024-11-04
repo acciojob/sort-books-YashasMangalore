a689a0542dcb471e508e78984268606741d648d7
Yashas <yashasjob@gmail.com>
2024-11-04 22:11:15 +0530

    c2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import store from "./components/Store"; // Assuming this is your Redux store
+import { Provider } from "react-redux"; // Import Provider from react-redux
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import store from "./components/Store";
-import { Provider } from "react-redux";
-
+// Render the app with Redux Provider
 ReactDOM.render(
   <Provider store={store}>
     <App />
